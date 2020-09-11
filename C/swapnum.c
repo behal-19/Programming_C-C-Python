d0490398b5b2bb5962935b038b72580e005a4136
Yukti <71033864+behal-19@users.noreply.github.com>
2020-09-11 11:19:44 -0400

    Create swapnum.c

---
+++ C/swapnum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    printf("enter:");
+    scanf("%d",&a);
+    printf("enter:");
+    scanf("%d",&b);
+    printf("The swapped numbers are: %d and %d",b,a);
+    return 0;
+}
