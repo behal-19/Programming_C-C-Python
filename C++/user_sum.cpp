7f634f22d3a2083935a0c2887aa9f018bbef55c8
Yukti <71033864+behal-19@users.noreply.github.com>
2020-09-10 23:30:56 -0400

    Create user_sum.cpp

---
+++ C++/user_sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int num1,num2,y;
+    cout<<"Enter:";
+    cin>>num1;
+    cout<<"Enter:";
+    cin>>num2;
+    y=num1+num2;
+    cout<<"Result:"<<y;
+    return 0;
+}
