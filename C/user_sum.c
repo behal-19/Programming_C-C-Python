65dbb1c0d090e8751f58594536842010c3b173a8
Yukti <71033864+behal-19@users.noreply.github.com>
2020-09-10 23:19:25 -0400

    Create user_sum.c

---
+++ C/user_sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,y;
+    printf("Enter:");
+    scanf("%d",&num1);
+    printf("Enter:");
+    scanf("%d",&num2);
+    y=num1+num2;
+    printf("The sum is %d",y);
+    return 0;
+    
+}
