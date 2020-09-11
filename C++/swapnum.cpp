4da32047b203f42727ad6ac0625f3d9afd243fbc
Yukti <71033864+behal-19@users.noreply.github.com>
2020-09-11 11:17:07 -0400

    Create swapnum.cpp

---
+++ C++/swapnum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter:";
+    cin>>a;
+    cout<<"Enter:";
+    cin>>b;
+    cout<<"Before swapping a=" <<a<<"b= "<<b<<endl;
+    cout<<"After swapping a="<<b<<"b="<<a<<endl;
+    return 0;
+}
