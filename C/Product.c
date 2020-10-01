6e30d9494ba5c183ec8eeec86b4d61b033003bb9
Yukti <71033864+behal-19@users.noreply.github.com>
2020-09-30 22:19:37 -0400

    Create Product.c

---
+++ C/Product.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\nEnter:");
+    scanf("%d",&a);
+    printf("\nEnter:");
+    scanf("%d",&b);
+    c=a*b;
+    printf("\nThe product is=%d",c);
+    return 0;
+}
