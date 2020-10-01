c5eb59b6c7be173c763b7975b95856fde1e11d67
Yukti <71033864+behal-19@users.noreply.github.com>
2020-09-30 22:19:55 -0400

    Create XML.c

---
+++ C/XML.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char c1='X',c2='M',c3='L';
+    printf("Enter:%c\n",c1);
+    printf("Enter:%c\n",c2);
+    printf("Enter:%c\n",c3);
+    printf("The output for %c%c%c is %c%c%c",c1,c2,c3,c3,c2,c1);
+    return 0;
+}
