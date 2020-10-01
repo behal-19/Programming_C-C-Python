f1526bf84cb6ba1399d726d4af39283dd16e8bbf
Yukti <71033864+behal-19@users.noreply.github.com>
2020-10-01 09:25:31 -0400

    Create Month.c

---
+++ C/Month.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main ()
+{
+    int x;
+    printf("Enter:");
+    scanf("%d",&x);
+    if(x==1)
+    {
+        printf("January");
+    }
+    else if(x==2)
+    {
+        printf("Feb");
+    }
+    else if(x==3)
+    {
+        printf("March");
+    }
+    else if(x==4)
+    {
+        printf("April");
+    }
+    else if(x==5)
+    {
+        printf("May");
+    }
+    else if(x==6)
+    {
+        printf("June");
+    }
+    else if(x==7)
+    {
+        printf("July");
+    }
+    else if(x==8)
+    {
+        printf("Aug");
+    }
+    else if(x==9)
+    {
+        printf("Sep");
+    }
+    else if(x==10)
+    {
+        printf("Oct");
+    }
+    else if(x==11)
+    {
+        printf("November");
+    }
+    else
+    {
+        printf("December");
+    }
+    return 0;
+}
